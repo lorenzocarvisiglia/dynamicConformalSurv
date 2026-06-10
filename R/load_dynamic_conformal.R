a1f3f0e51d705aa3141ec8f0a15782dd4c250356
lollo19999 <131760713+lollo19999@users.noreply.github.com>
2026-06-10 17:20:36 +0200

    Add function loader

---
+++ R/load_dynamic_conformal.R
@@ -0,0 +1,6 @@
+# Load all functions needed for dynamic conformal prediction intervals
+
+source("R/working_prc_model.R")
+source("R/conformal_calibration.R")
+source("R/survival_inversion.R")
+source("R/dynamic_conformal_pi.R")
