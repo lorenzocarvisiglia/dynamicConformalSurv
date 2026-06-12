c4c86cda8ceb7abadbdc658fd933a657b1e6f670
lollo19999 <131760713+lollo19999@users.noreply.github.com>
2026-06-12 19:30:26 +0200

    load_dynamic_conformal.R

--- R/load_dynamic_conformal.R
+++ R/load_dynamic_conformal.R
@@ -1,6 +1,3 @@
-# Load all functions needed for dynamic conformal prediction intervals
+# Load dynamic conformal prediction interval functions
 
-source("R/working_prc_model.R")
-source("R/conformal_calibration.R")
-source("R/survival_inversion.R")
 source("R/dynamic_conformal_pi.R")
